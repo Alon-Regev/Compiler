183fb1193e9a7b4a9dd2df902039fcf9fc2615fe
Alon Regev <alonr3704@gmail.com>
2022-04-08 21:58:57 +0300

    added block node

---
+++ AST_Nodes/Block.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler
+{
+	class Block : Statement
+	{
+		public Block(int line) : base(line)
+		{
+		}
+
+		// Method adds a statement to the block
+		// input: statement to add
+		// return: none
+		public void AddStatement(Statement statement)
+		{
+			AddChild(statement);
+		}
+
+		// Method gets a statement from the block based on the index
+		// input: index
+		// return: statement
+		public Statement GetStatement(int i)
+		{
+			return (Statement)GetChild(i);
+		}
+	}
+}
