3339a62469589f3a3bce6cd6fccae5291a8d71d6
Alon Regev <alonr3704@gmail.com>
2022-04-08 13:44:08 +0300

    added statement nodes

---
+++ AST_Nodes/ExpressionStatement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler
+{
+	class ExpressionStatement : Statement
+	{
+		public ExpressionStatement(Expression expr) : base(expr.Line)
+		{
+			AddChild(expr);
+		}
+
+		// Getter for expression
+		public Expression GetExpression()
+		{
+			return (Expression)GetChild(0);
+		}
+
+		// ToString override specifies this is an expression
+		public override string ToString(int indent)
+		{
+			return " Expr Stmt" + base.ToString(indent);
+		}
+	}
+}
