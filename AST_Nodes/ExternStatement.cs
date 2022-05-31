0a66b20e96f91cef6d92eb9d37e8475fa2e7e1fb
Alon Regev <alonr3704@gmail.com>
2022-06-01 00:10:18 +0300

    added extern stmt node

---
+++ AST_Nodes/ExternStatement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler
+{
+	class ExternStatement : Statement
+	{
+		public string Identifier { private set; get; }
+
+		public ExternStatement(Token identifier) : base(identifier.Line)
+		{
+			Identifier = identifier.Value;
+		}
+
+		public override string ToString(int indent)
+		{
+			return "Extern Statement" + base.ToString(indent);
+		}
+	}
+}
