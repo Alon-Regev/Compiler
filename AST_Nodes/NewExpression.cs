049236a660ae326846556b151a4547ccda47b193
Alon Regev <alonr3704@gmail.com>
2022-06-11 23:46:42 +0300

    added new and delete, parsing and semantic analysis

---
+++ AST_Nodes/NewExpression.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler
+{
+	class NewExpression : Expression
+	{
+		public NewExpression(ValueType type, Expression size) : base(size.Line)
+		{
+			Type = type;
+			Type.Pointer++;
+			AddChild(size);
+		}
+
+		public Expression Size()
+		{
+			return GetChild(0) as Expression;
+		}
+
+		// ToString override specifies this is an expression
+		public override string ToString(int indent)
+		{
+			return "New" + base.ToString(indent);
+		}
+	}
+}
